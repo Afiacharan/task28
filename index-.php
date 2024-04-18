07058816cc7631f89ed18c64aa716166c2b801e2
Afiacharan <162119505+Afiacharan@users.noreply.github.com>
2024-04-18 16:51:26 +0500

    Add files via upload

---
+++ index-.php
@@ -0,0 +1,30 @@
+<?php
+$name="<h1 style='text-align:center ; color:black;  background-color:  rgba(46, 91, 117, 0.788);  border: 3px solid'>AFIA ChARAN</h1>";
+$Fname="<h2 style='text-align:center ;  background-color:  rgba(117, 46, 113, 0.445);  border: 3px solid' >Nisar ahmed</h2>";
+$brother="<h2 style='text-align:center;  background-color:  rgba(117, 46, 50, 0.445);  border: 3px solid'> big brother nasir nisar</h2>";
+$cast="<h3 style='text-align:center; background-color: rgba(196, 25, 219, 0.788); border: 3px solid'> my cast charan</h3>";
+$sistername1="<h3 style='text-align:center ; background-color: rgba(219, 25, 138, 0.788)' border: 3px solid>farhana is my big sister </h3>";
+$sistername2="<h3 style='text-align:center;  background-color: rgba(168, 196, 43, 0.788); border: 3px solid'>rufina is second number sister </h3>";
+$sistername3="<h3 style='text-align:center ;background-color: rgba(56, 196, 43, 0.788); border: 3px solid'>sofia is third number sister</h3>";
+$sistername4="<h3 style='text-align:center ;background-color:  rgba(196, 91, 43, 0.788); border: 3px solid'>salma is forth and last sister </h3>";
+$merijaan="<h3 style='text-align:center ;background-color:  rgba(196, 91, 43, 0.788); border: 3px solid ; '>  meri jaan umefarwa and zunaira</h3>";
+ echo $name;
+ echo "<hr>";
+ echo $Fname;
+ echo "<hr>";
+ echo $brother;
+ echo "<hr>";
+ echo $cast;
+ echo "<hr>";
+ echo $sistername1;
+ echo "<hr>";
+ echo $sistername2;
+ echo "<hr>";
+ echo $sistername3;
+ echo "<hr>";
+ echo $sistername4;
+ echo "<hr>";
+ echo $merijaan;
+
+
+?>
